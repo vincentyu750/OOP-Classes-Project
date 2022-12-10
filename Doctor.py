5ba02cb41030614e28799390b51825b4be3a0096
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2022-12-09 21:25:47 -0800

    Changes to Doctors and Management
    Finished Doctors.py file and main outline for the management.py file.
    
    Tested test cases for doctors.py

--- Doctor.py
+++ Doctor.py
@@ -71,11 +71,14 @@ def addDrToFile(doctor):
     #adding the doctor into the file
     f.write("\n" + string)
 
+    #closing the file
+    f.close()
+
 """
 Enter Doctor Info
 Function: Asks the user to enter doctor properties 
 """
-def enterDrInfo(list):
+def enterDrInfo():
     #Asking the user for the doctors information then creating a doctor object to add to the list
     id = input("Enter the doctor's ID:\n\n")
     name = input("Enter the doctor's name:\n\n")
@@ -108,7 +111,7 @@ doctors/file using the doctor ID that the user enters
 """
 def searchDoctorById(list):
     #Asks the user for the ID
-    selected_id = input("Please enter the id of the doctor that you want to edit their information:\n\n")
+    selected_id = input("Please enter the doctor id:\n\n")
     
     #if the id is not there
     is_there = 0
@@ -133,7 +136,7 @@ doctors/file using the doctor name that the user enters
 """
 def searchDoctorByName(list):
     #Asks the user for the Name
-    selected_name = input("Please enter the name of the doctor that you want to edit their information:\n\n")
+    selected_name = input("Please enter the name of the doctor:\n\n")
 
     #if the id is not there
     is_there = 0
@@ -157,9 +160,37 @@ Function: Asks the user to enter the ID of the doctor to
 change their information, and then the user can enter the 
 new doctor information
 """
-def editDoctorInfo(self):
-    return 0
+def editDoctorInfo(list):
+    
+    #Asks the user for the ID
+    selected_id = input("Please enter the id of the doctor that you want to edit their information:\n\n")
+    
+    #if the id is not there
+    is_there = 0
 
+    #Traverses through the list checking if the id is matched
+    for i in range(0, len(list)):
+        if str(list[i].id) == selected_id:
+
+            #changes is there to 1 signify that it is there
+            is_there = 1
+
+            #Asking the user for the doctors information then creating a doctor object to add to the list
+            id = selected_id
+            name = input("Enter the doctor's name:\n\n")
+            spec = input("Enter the doctor's speciality:\n\n")
+            time = input("Enter the doctor's timing (e.g., 7am-10pm):\n\n")
+            qual = input("Enter the doctor's qualification:\n\n")
+            room = input("Enter the doctor's room number:\n\n")
+            
+            #creating a doctors object with the values asked
+            inputDoctor = Doctor(id,name,spec,time,qual,room)
+
+            #replaces the current selected with the new updated doctor
+            list[i] = inputDoctor
+
+    if is_there == 0:
+        print("Can't find the doctor with the same name on the system")
     
 """
 Display Doctors List
@@ -207,11 +238,217 @@ def readDoctorsFile():
             #adding the newly created doctor objects into a list
             doctorList.append(doctor)
 
+    #closing the file
+    f.close()
+
     return doctorList     
 
-print("Doctors Testing")
-listOfDoctorsMain = readDoctorsFile()
-writeListOfDoctorsToFile(listOfDoctorsMain)
-displayDoctorsList()
-searchDoctorById(listOfDoctorsMain)
-searchDoctorByName(listOfDoctorsMain)
+#Testing the doctors case
+def doctorMenu():
+
+    #creating a list from the file
+    listOfDoctors = readDoctorsFile()
+
+    #start of the menu selection
+    doctorMenuSelection = True
+    while doctorMenuSelection:
+
+        #Doctor menu prompt
+        choice = int(input("Doctors Menu:\n1 - Display Doctors list\n2 - Search for doctor by ID\n3 - Search for doctor by name\n4 - Add doctor\n5 - Edit doctor into\n6 - Back to the Main Menu\n\n"))
+
+        #menu selection
+        if choice == 1: 
+            displayDoctorsList()
+
+        if choice == 2:
+            searchDoctorById(listOfDoctors)
+
+        if choice == 3:
+            searchDoctorByName(listOfDoctors)
+
+        if choice == 4:
+            inputDoctor = enterDrInfo()
+            addDrToFile(inputDoctor)
+
+        if choice == 5:
+            editDoctorInfo(listOfDoctors)
+            writeListOfDoctorsToFile(listOfDoctors)
+
+        if choice == 6:
+            doctorMenuSelection = False
+
+"""
+Expected output (tested)
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+1
+Id   Name                   Speciality      Timing          Qualification   Room Number
+
+21   Dr.Gody                ENT             5am-11aM        MBBS,MD         17
+       
+32   Dr.Vikram              Physician       10pm-3am        MBBS,MD         45
+       
+17   Dr.Amy                 Surgeon         8pm-2am         BDM             8
+        
+33   Dr.David               Artho           10am-4pm        MBBS,MS         40
+       
+123  Dr. Ross               Headackes       8pm-10am        MST             102
+      
+66   Dr. Mike               Heart           9am-5pm         MS              2         
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+2
+
+ Enter the doctor Id: 
+
+66
+Id   Name                   Speciality      Timing          Qualification   Room Number
+
+66   Dr. Mike               Heart           9am-5pm         MS              2         
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+3
+
+ Enter the doctor name: 
+
+Dr.David
+Id   Name                   Speciality      Timing          Qualification   Room Number
+
+33   Dr.David               Artho           10am-4pm        MBBS,MS         40
+       
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+2
+
+ Enter the doctor Id: 
+
+20
+Can't find the doctor with the same ID on the system
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+3
+
+ Enter the doctor name: 
+
+Dr. Tom
+Can't find the doctor with the same name on the system
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+4
+Enter the doctor’s ID:
+
+62
+Enter the doctor’s name:
+
+Dr. Smith
+Enter the doctor’s specility:
+
+Heart
+Enter the doctor’s timing (e.g., 7am-10pm):
+
+6am-11am
+Enter the doctor’s qualification:
+
+PHD
+Enter the doctor’s room number:
+
+12
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+5
+Please enter the id of the doctor that you want to edit their information: 
+
+66
+
+Enter new Name: 
+
+Dr. Mike kale
+
+Enter new Specilist in:
+
+Heart
+
+Enter new Timing: 
+
+9am-3pm
+
+Enter new Qualification: 
+
+MS
+
+Enter new Room number: 
+
+2
+
+Back to the prevoius Menu
+
+Doctors Menu:
+1 - Display Doctors list
+2 - Search for doctor by ID
+3 - Search for doctor by name
+4 - Add doctor
+5 - Edit doctor info
+6 - Back to the Main Menu
+
+6
+"""
