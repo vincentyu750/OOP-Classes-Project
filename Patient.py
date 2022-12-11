a395234c908fe170892587bd107a0ef0c542c6ca
vincentyu750 <70932487+vincentyu750@users.noreply.github.com>
2022-12-11 16:18:17 -0700

    Implemented Patient.py to management
    Successfully implemented and tested patient menu and patient functions from within the management Menu
    
    (Anthony created the patient.py file)
    I helped him implement it into the main python repository

--- Patient.py
+++ Patient.py
@@ -157,3 +157,30 @@ class Patient:
         patients_file.write(self.formatPatientInfo(patient_to_add))
         patients_file.write('\n')
         patients_file.close()
+
+def patientMenu():
+
+    my_patient = Patient()
+    isRunning = True
+
+    while isRunning:
+
+        #list of doctors
+        patient_list = my_patient.readPatientsFile()
+
+        patients_menu = int(input("Patients Menu:\n1 - Display patients list\n2 - Search for patient by ID\n3 - Add patient\n4 - Edit patient info\n5 - Back to the Main Menu\n"))
+
+        if patients_menu == 1:
+            my_patient.displayPatientInfo()
+            print("Back to previous menu\n")
+        if patients_menu == 2:
+            my_patient.searchPatientByID()
+            print("Back to previous menu\n")
+        if patients_menu == 3:
+            my_patient.addPatientToFile()
+            print("Back to previous menu\n")
+        if patients_menu == 4:
+            my_patient.editPatientInfo(patient_list)
+            print("Back to previous menu\n")
+        if patients_menu == 5:
+            isRunning = False
