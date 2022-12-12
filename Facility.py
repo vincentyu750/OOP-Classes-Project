c8542971111ba671cb49fe37b4ba432757a6751a
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2022-12-11 21:51:59 -0800

    Added Darien's part of the code to main code
    Added Laboratories code and Facilities Code

--- Facility.py
+++ Facility.py
@@ -11,19 +11,39 @@ class Facility:
     Add Facility
     Function: Adds and writes the facility name to the file
     """
-    def addFacility():
-        return 0
+    def addFacility(self):
+        newfacilityname = self.facilityName
+        facilityfile = open("facilities.txt", "a+")
+        facilityfile.write("\n" + newfacilityname)
+        facilityfile.close()
+
+    """
+    Make List of Facilities
+    Function: Opens Text file to make a list of Facilities Objects
+    """
+    def makeListOfFacilities(self):
+        facilityfile = open("facilities.txt", "r")
+        content = facilityfile.read()
+        facility_list = content.split("\n")
+        facilityfile.close()
+        return facility_list
 
     """
     Display Facilities
     Function: Displays the list of facilities
     """
-    def displayFacilities():
-        return 0
+    def displayFacilities(self):
+        facility_list = self.makeListOfFacilities()
+        print(facility_list)
 
     """
     Write List of Facilities to File
     Function: Writes the facilities list to facilities.txt
     """
-    def writeListofFacilitiesToFile():
-        return 0
+    def writeListofFacilitiesToFile(self):
+        content = self.makeListOfFacilities()
+        facilities_list = ('\n'.join(content))
+        print(facilities_list)
+        faciltyfile = open("facilities.txt", "r+")
+        faciltyfile.write(facilities_list)
+        faciltyfile.close()
--- Laboratory.py
+++ Laboratory.py
@@ -14,44 +14,63 @@ class Laboratory:
     Function: Adds and writes the lab name to the file in the format
     of the data that is in the file
     """
-    def addLabToFile():
-        return 0
+    def addlabtofile(self):
+        newlabname = input("What is the name of the lab that you would like to add?")
+        newlabcost = input("what is the cost of this new lab?")
+        labfile = open("laboratories.txt", "a+")
+        labfile.write("\n" + newlabname + "_" + newlabcost)
+        labfile.close()
 
     """
     Write List of Labs to File
     Function: Writes the list of labs into the files
     laboratories.txt
     """
-    def writeListOfLabsToFile():
-        return 0
+    def writelistoflabstofile(self):
+        content = labhere.readlaboratoriesfile()
+        labs_list = ('\n'.join(content))
+        print(labs_list)
+        labfile = open("laboratories.txt", "r+")
+        labfile.write(labs_list)
+        labfile.close()
         
     """
     Displays Labs List
     Function: Displays the list of laboratories
     """
-    def displayLabsList():
-        return 0
+    def displaylabslist(self):
+        labs_list = labhere.readlaboratoriesfile()
+        print(labs_list)
 
     """
     Format Lab Info
     Function: Formats the laboratory object similar to the 
     laboratories.txt file
     """
-    def formatLabInfo():
-        return 0
+    def formatlabinfo(self, labname, cost):
+        specificlab = (labname + "_" + cost)
+        return specificlab
 
     """
     Emter Laboratory Info
     Function: Asks the user to enter lab name and cost 
     and forms a laboratory object
     """
-    def enterLaboratoryInfo():
-        return 0
+    def enterlaboratoryinfo(self):
+        newlabname = input("What is the name of the lab that you would like to add?")
+        newlabcost = input("what is the cost of this new lab?")
+        newlab = labhere.formatlabinfo(newlabname, newlabcost)
+        list = labhere.readlaboratoriesfile()
+        list.append(newlab)
 
     """
     Read Laboratories File
     Function: Read the laboratories.txt file and fills its contents
     in a list of Laboratory objects
     """
-    def readLaboratoriesFile():
-        return 0
+    def readlaboratoriesfile(self):
+        labfile = open("laboratories.txt", "r")
+        content = labfile.read()
+        labs_list = content.split("\n")
+        labfile.close()
+        return labs_lis
